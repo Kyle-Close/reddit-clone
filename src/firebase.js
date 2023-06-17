3a7ea9510ca03f1d180ee0e123de5ae865784872
Kyle-Close <k.james.close@gmail.com>
2023-06-17 15:31:58 -0700

    Firebase deploy working.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBu9YWRwQ9eutVyIrIha8dxe9CAXNgrQEM',
+	authDomain: 'reddit-clone-3b1ef.firebaseapp.com',
+	projectId: 'reddit-clone-3b1ef',
+	storageBucket: 'reddit-clone-3b1ef.appspot.com',
+	messagingSenderId: '512565635955',
+	appId: '1:512565635955:web:9daf2e091a6cbc43763306',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
