bd52dc0591eef2f5b956d76279edef3c980d13dd
Kyle-Close <k.james.close@gmail.com>
2023-06-17 15:42:57 -0700

    React router imported and working. This should be all the setup done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
 
+import './index.css';
 import { Provider } from 'react-redux';
 import store from './reducers';
+import router from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<RouterProvider router={router} />
 		</Provider>
 	</React.StrictMode>
 );
