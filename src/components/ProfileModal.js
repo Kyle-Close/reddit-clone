8c2786794b11dde6c7b64c22dea7c48c1a253dd7
Kyle-Close <k.james.close@gmail.com>
2023-06-19 20:14:43 -0700

    Working on logged in profile modal

--- src/components/ProfileModal.js
+++ src/components/ProfileModal.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 import PopoutModal from '../components/PopoutModal';
-import snoo from '../img/snoo.png';
+import snooLoggedOut from '../img/snoo.png';
+import snooLoggedIn from '../img/snoo-logged-in.png';
+import karma from '../img/karma.svg';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { setModalDirection } from '../reducers/modal';
+
+import { logout } from '../auth';
+import { auth } from '../firebase';
 
 function ProfileModal({ direction }) {
 	const navigate = useNavigate();
 	const modal = useSelector((state) => state.modal);
 	const authState = useSelector((state) => state.authState);
 	const dispatch = useDispatch();
+	const [modalContent, setModalContent] = useState(null);
 
 	useEffect(() => {
 		// Set the modal direction and isOpen property
@@ -21,14 +27,14 @@ function ProfileModal({ direction }) {
 	}, [direction, authState]);
 
 	const getProfileModalContents = () => {
-		// Not logged in
+		// Logged in
 		if (authState && authState.userId !== null) {
 			return (
 				<div className='flex flex-col items-center w-full h-full'>
 					<div className='mt-12 w-1/2 aspect-auto flex justify-center'>
 						<img
 							className='object-cover'
-							src={snoo}
+							src={snooLoggedOut}
 						/>
 					</div>
 					<p className='mt-8 w-full text-gray-300 text-sm text-center'>
@@ -42,7 +48,8 @@ function ProfileModal({ direction }) {
 					</button>
 					<p className='mt-10 text-gray-300 text-xs'>Don't have an account?</p>
 					<button
-						onClick={() => navigate('/signup')}
+						//onClick={() => navigate('/signup')}
+						onClick={logout(auth)}
 						className='text-center text-xs mt-4 bg-orange-600 text-gray-300 w-2/3 h-8 rounded-full'
 					>
 						Sign up
@@ -50,19 +57,42 @@ function ProfileModal({ direction }) {
 				</div>
 			);
 		} else {
-			/* console.log('User is logged in'); */
+			console.log('Logged in...');
+			return (
+				<div className='w-full h-full flex flex-col items-center'>
+					<div className='mt-8'>
+						<img src={snooLoggedIn} />
+					</div>
+					<p className='mt-4 text-gray-200'>u/close55</p>
+					<div className='mt-8 flex w-full px-4 text-gray-200'>
+						<div className='grow flex gap-2'>
+							<div className='w-12 aspect-auto'>
+								<img src={karma} />
+							</div>
+							<div className='grow'>
+								<h4>11</h4>
+								<p>Karma</p>
+							</div>
+						</div>
+						<div className='grow'>Reddit Age</div>
+					</div>
+				</div>
+			);
 		}
 	};
 
 	// Display correct contents based on if user is logged in
-	const modalContent = React.useMemo(
-		() => getProfileModalContents(),
-		[authState]
-	);
+	React.useEffect(() => {
+		console.log('Logged in');
+		const modalContent = getProfileModalContents();
+		setModalContent(modalContent);
+	}, [authState]);
 
 	/* console.log('Latest modal content', modalContent); */
 
-	return <PopoutModal modal={modal}>{modalContent}</PopoutModal>;
+	return (
+		<PopoutModal modal={modal}>{modalContent && modalContent}</PopoutModal>
+	);
 }
 
 export default ProfileModal;
