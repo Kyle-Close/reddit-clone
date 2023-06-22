3fed23b336bc3a6012b76f9691a58b0f2f4ecdd6
Kyle-Close <k.james.close@gmail.com>
2023-06-21 21:58:42 -0700

    styled create post page

---
+++ src/components/header/PostButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PostButton() {
+	return (
+		<button className='bg-blue-400 text-blue-50 px-6 py-3 rounded-full h-2/3 flex justify-center items-center text-md font-bold'>
+			Post
+		</button>
+	);
+}
+
+export default PostButton;
--- src/components/subreddit-page/CreatePost.js
+++ src/components/subreddit-page/CreatePost.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 import Header from '../header/Header';
 import BackButton from '../header/BackButton';
+import PostButton from '../header/PostButton';
 
 function CreatePost() {
+	const { subredditName } = useParams();
+
 	function handleInputChange(e) {
 		console.log(e.target.value);
 	}
 	return (
-		<div className='flex flex-col h-screen bg-black'>
-			<Header justify='justify-between'>
+		<div className='flex flex-col h-screen bg-black text-gray-200'>
+			<Header gap='gap-12'>
 				<BackButton />
+				<h1 className='font-semibold text-lg'>{`/r/${subredditName.toLowerCase()}`}</h1>
+				<div className='grow h-full flex justify-end items-center mr-6'>
+					<PostButton />
+				</div>
 			</Header>
 
 			<div className='mx-8 my-6 flex flex-col gap-6 h-full'>
 				<input
+					maxLength={50}
 					onChange={handleInputChange}
 					className='bg-zinc-600 h-12 rounded-md px-3 text-white placeholder-gray-400'
 					placeholder='Title'
