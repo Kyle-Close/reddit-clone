20d897f8cf1be447fa12542c07ec6a53782534cb
Kyle Close <k.james.close@gmail.com>
2023-06-23 13:12:42 -0400

    downvoting working. Refactored a bit

--- src/components/subreddit-page/DownvoteButton.js
+++ src/components/subreddit-page/DownvoteButton.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import DownvotesIcon from '../../img/downvote-icon.png';
 import { downvotePost } from '../../firebase';
 
 function DownvoteButton({ numDownvotes, postId }) {
+	const authState = useSelector((state) => state.authState);
+
 	async function handleDownvoteClick(e) {
 		e.stopPropagation();
-		downvotePost(postId);
+		if(!authState) return
+		if(await downvotePost(postId, authState.userId)){
+			console.log("Success! Post downvoted")
+		}else{
+			console.log("Error. Could not downvote post")
+		}
 	}
+
 	return (
 		<button
 			onClick={handleDownvoteClick}
--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,10 @@ import {
 	addDoc,
 	updateDoc,
 	increment,
+	arrayUnion,
+	arrayRemove
 } from 'firebase/firestore';
+import { async } from 'q';
 import { v4 as uuidv4 } from 'uuid';
 
 // Your web app's Firebase configuration
@@ -171,15 +174,52 @@ export async function getPostDataFromPostId(postId) {
 	}
 }
 
-export async function downvotePost(postId) {
-	const q = query(collection(db, 'posts'), where('postId', '==', postId));
-	const querySnapshot = await getDocs(q);
+function hasUserDownvotedPost(downvoteUsers, userId){
+    if(!Array.isArray(downvoteUsers) || downvoteUsers.length < 1) return false
+    if(downvoteUsers.includes(userId)) {
+        console.log('User already downvoted this post')
+        return true
+    }
+    else return false
+}
 
-	querySnapshot.forEach(async (docSnapshot) => {
-		const postRef = doc(db, 'posts', docSnapshot.id);
+function hasUserUpvotedPost(upvoteUsers, userId){
+    if(!Array.isArray(upvoteUsers) || upvoteUsers.length < 1) return false
+    if(upvoteUsers.includes(userId)) {
+        console.log('User already upvoted this post')
+        return true
+    }
+    else return false
+}
+
+export async function downvotePost(postId, userId) {
+    // 1. Find the post the is being downvoted based on id
+    const postIdQuery = query(collection(db, 'posts'), where('postId', '==', postId));
+    const postSnapshot = await getDocs(postIdQuery); // Snapshot of all docs
+	const docSnapshot = postSnapshot.docs[0]; // Snapshot of just first doc
+	const postRef = doc(db, 'posts', docSnapshot.id);
+
+    // 2. Check if user has already downvoted. If yes return
+    if(hasUserDownvotedPost(docSnapshot.data().downvoteUsers, userId)) return;
+
+	// 3. Check if the user has already upvoted. If yes, remove the upvote
+	if(hasUserUpvotedPost(docSnapshot.data().upvoteUsers, userId)){
+		// Remove upvote
+		console.log(userId)
 		await updateDoc(postRef, {
-			downvotes: increment(1),
+			upvoteUsers: arrayRemove(userId)
 		});
-		console.log('Document successfully updated!');
+	}
+
+	// 4. Can now safely add downvote
+	await updateDoc(postRef, {
+		downvotes: increment(1),
 	});
+
+	// 5. Add user id to downvotes downvoteUsers
+	await updateDoc(postRef, {
+		downvoteUsers: arrayUnion(userId)
+	});
+
+	return true
 }
